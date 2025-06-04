08e2669d7a277aac049ccd119bbcf066c3f57ac6
pradhokot <yoggi60@gmail.com>
2025-06-04 09:57:01 +0700

    2025 juni 4, update 1

--- templates/template1/layout/trigger-search.js
+++ templates/template1/layout/trigger-search.js
@@ -1,4 +1,4 @@
 document.writeln(`
-<button class="btn rounded-0 border-0 shadow-none py-lg-2 px-lg-3 text-reset link-hover">
+<button class="btn rounded-0 border-0 shadow-none p-0 py-lg-2 px-lg-3 text-reset link-hover">
    <i class="fas fa-search fa-lg"></i>
 </button>`)
