58b0e8c729ffff480d02f12f59bb373929afdcbb
pradhokot <yoggi60@gmail.com>
2025-06-09 15:36:43 +0700

    2025 juni 9, update 6

---
+++ templates/template1/layout/banner-career.js
@@ -0,0 +1,26 @@
+document.writeln(`
+<div class="banner">
+   <div class="container-lg px-0 px-lg-3 pt-lg-2">
+      <div class="jumbotron" id="banner-career" style="background-color: #eceff1;">
+         <div class="row g-0 align-items-lg-end">
+            <div class="col col-12 col-md-8 col-lg-5 pb-0 pe-lg-0 py-lg-5 my-lg-auto" style="padding: calc(3rem + 1vw);">
+               <div class="banner-wording">
+                  <h5 class="fs-1">
+                     <b class="fw-medium">
+                        Career at INDRACO
+                     </b>
+                  </h5>
+                  <p class="lead mb-0">
+                     Know the flow of the recruitment process at INDRACO from sending applications to the employee acceptance stage.
+                     <br>
+                     Come join and be a part of us! 
+                  </p>
+               </div>
+            </div><!-- end col -->
+            <div class="col col-12 col-lg-7">
+               <img src="../../asset/img/career-banner.png" width="100%" height="auto" alt="">
+            </div><!-- end col -->
+         </div><!-- end row -->
+      </div><!-- end banner career -->
+   </div><!-- end container -->
+</div>`)
