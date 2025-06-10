7b773a6db363e05b65217a80a5f9ea33819ac570
pradhokot <yoggi60@gmail.com>
2025-06-10 10:50:54 +0700

    2025 juni 10, update 4

---
+++ templates/template1/layout/modal-search.js
@@ -0,0 +1,87 @@
+document.writeln(`
+<div id="modal-search" class="modal fade" tabindex="-1">
+   <div class="modal-dialog modal-fullscreen-md-down modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+         <div class="modal-header">
+            <div class="input-group">
+               <button class="btn border-dark border-2 border-end-0">
+                  <i class="fas fa-search"></i>
+               </button>
+               <input type="search" class="form-control border-dark border-2 border-start-0 ps-0" placeholder="Search">
+            </div>
+            <button class="btn btn-lg border-0 shadow-none d-md-none" data-bs-dismiss="modal">
+               Cancel
+            </button>
+         </div>
+         <div class="modal-body d-flex flex-column row-gap-4">
+            <header>
+               <h5 class="fs-reset"><b class="fw-medium">SEARCH</b></h5>
+               <div class="d-flex flex-wrap gap-1">
+                  <button class="btn btn-sm btn-light">jaheku <i class="fas fa-close opacity-50 ms-2"></i></button>
+                  <button class="btn btn-sm btn-light">BROCHOCO <i class="fas fa-close opacity-50 ms-2"></i></button>
+                  <button class="btn btn-sm btn-light">tugu buaya <i class="fas fa-close opacity-50 ms-2"></i></button>
+                  <button class="btn btn-sm btn-light">supresso <i class="fas fa-close opacity-50 ms-2"></i></button>
+               </div>
+            </header>
+            <main>
+               <p class="m-0 opacity-50">
+                  <b class="fw-medium"><small><small>4 Result</small></small></b>
+               </p>
+               <div>
+                  <hr class="opacity-100 mt-0">
+                  <ul class="list-group list-group-flush small">
+                     <li class="list-group-item px-0">
+                        <a href="#" class="text-reset text-decoration-none link-hover">
+                           <h5 class="fs-reset">
+                              JAHAKU (HANGATKAN HARIMU)
+                           </h5>
+                           <p>
+                              Jaheku mengantarkan rasa hangat jahe ke dalam rasa dan aroma harum kopi kesukaan Anda. Paduan dua rasa kuat ini memberikan khasiat yang asli dan juga kenangan yang kuat.
+                           </p>
+                        </a>
+                     </li>
+                     <li class="list-group-item px-0">
+                        <a href="#" class="text-reset text-decoration-none link-hover">
+                           <h5 class="fs-reset">
+                              JAHAKU (HANGATKAN HARIMU)
+                           </h5>
+                           <p>
+                              Jaheku mengantarkan rasa hangat jahe ke dalam rasa dan aroma harum kopi kesukaan Anda. Paduan dua rasa kuat ini memberikan khasiat yang asli dan juga kenangan yang kuat.
+                           </p>
+                        </a>
+                     </li>
+                     <li class="list-group-item px-0">
+                        <a href="#" class="text-reset text-decoration-none link-hover">
+                           <h5 class="fs-reset">
+                              JAHAKU (HANGATKAN HARIMU)
+                           </h5>
+                           <p>
+                              Jaheku mengantarkan rasa hangat jahe ke dalam rasa dan aroma harum kopi kesukaan Anda. Paduan dua rasa kuat ini memberikan khasiat yang asli dan juga kenangan yang kuat.
+                           </p>
+                        </a>
+                     </li>
+                     <li class="list-group-item px-0">
+                        <a href="#" class="text-reset text-decoration-none link-hover">
+                           <h5 class="fs-reset">
+                              JAHAKU (HANGATKAN HARIMU)
+                           </h5>
+                           <p>
+                              Jaheku mengantarkan rasa hangat jahe ke dalam rasa dan aroma harum kopi kesukaan Anda. Paduan dua rasa kuat ini memberikan khasiat yang asli dan juga kenangan yang kuat.
+                           </p>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+            </main>
+            <footer class="small">
+               <h5 class="fs-reset"><b class="fw-medium">Suggestion :</b></h5>
+               <ol>
+                  <li>Make sure that all words are spelled correctly</li>
+                  <li>Try different keywords</li>
+                  <li>Try different keywords</li>
+               </ol>
+            </footer>
+         </div>
+      </div>
+   </div>
+</div><!-- end modal search -->`)
